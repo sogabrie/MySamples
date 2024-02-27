0d50f80159c7d957809a40bf88f75161f33f34b2
sogabri <gabrielyan.sos@mail.ru>
2024-02-27 20:32:13 +0400

    update

--- sort/src/MySort/MySorts.hpp
+++ sort/src/MySort/MySorts.hpp
@@ -2,13 +2,82 @@
 
 #include <algorithm>
 #include <iostream>
+#include <functional>
 
 namespace MySorts{
 
-template <typename IT>
-void heap(IT first, IT last)
+template <class T, typename C = std::less<> >
+void	life_up(std::vector<T> &cp, size_t i, C cmp = C{}) noexcept
 {
-    std::sort(first, last);
+	long long j = static_cast<long long>(i);
+	for (; ((j + 1) / 2 - 1) >= 0 ; )
+	{
+		if (cmp(cp[j],cp[(j + 1) / 2 - 1]))
+		{
+			std::swap(cp[(j + 1) / 2 - 1], cp[j]);
+			j = (j + 1) / 2 - 1;
+		}
+		else
+			break;
+	}
+}
+
+template <class T, typename IT, typename C = std::less<>>
+std::vector<T>	make_heap(IT first, IT last, C cmp = C{}) noexcept
+{
+	auto it = first;
+	size_t i = 0;
+	for (; it !=  last; ++i, ++it);
+	std::vector<T> cp;
+	cp.reserve(i);
+	for (i = 0, it = first; it != last; ++i, ++it)
+	{
+		cp.push_back(std::move((*it)));
+		life_up<T>(cp,i,cmp);
+	}
+	return cp;
+}
+
+template <class T, typename C = std::less<>>
+void	_find(std::vector<T> &cp, size_t i, C cmp = C{}) noexcept
+{
+	if (i > 0)
+	{
+		std::swap(cp[0], cp[i]);
+		for (size_t j = 0; j < i;)
+		{
+			if ((j + 1) * 2 - 1 < i && (j + 1) * 2 < i && \
+			cmp(cp[(j + 1) * 2], cp[(j + 1) * 2 - 1]))
+			{
+				std::swap(cp[(j + 1) * 2], cp[j]);
+				j = (j + 1) * 2;
+			}
+			else if((j + 1) * 2 - 1 < i)
+			{
+				std::swap(cp[(j + 1) * 2 - 1], cp[j]);
+				j = (j + 1) * 2 - 1;
+			}
+			else
+				break;
+		}
+		
+	}
+}
+
+template <class T, typename IT, typename C = std::less<>>
+void	pop_heap(IT first, std::vector<T> cp, C cmp = C{}) noexcept
+{
+	for (long long i = cp.size() - 1; i >= 0; --i, ++first)
+	{	
+		_find(cp, i,cmp);
+		*first = std::move(cp[i]);
+	}
+}
+
+template <class T, typename IT, typename C = std::less<>>
+void	heap(IT first, IT last, C cmp = C{}) noexcept
+{
+	pop_heap(first, std::move(make_heap<T>(first,last, cmp)), cmp);
 }
 
 }
--- sort/src/main.cpp
+++ sort/src/main.cpp
@@ -5,15 +5,26 @@
 int main()
 {
     std::vector<size_t> vecptr;
-    MySorts::Generator<std::vector<size_t> , 1000000 >::run(vecptr);
+    MySorts::Generator<std::vector<size_t> , 10000000 >::run(vecptr);
     MySorts::Duration::start();
-    MySorts::heap(vecptr.begin(), vecptr.end());
-    for (auto &v: vecptr)
-    {
-        std::cout << v << std::endl;
-    }
+    auto vecptr2 = vecptr;
+    std::cout << "Copy  time   = " << MySorts::Duration::end() << std::endl;
+    // for (auto &v: vecptr)
+    // {
+    //     std::cout << v << std::endl;
+    // }
 
-    std::cout << "time = " << MySorts::Duration::end() << std::endl;
-    
+    MySorts::Duration::start();
+    MySorts::heap<size_t>(vecptr.begin(), vecptr.end());
+    std::cout << "My sort time = " << MySorts::Duration::end() << std::endl;
+
+    MySorts::Duration::start();
+    std::sort(vecptr2.begin(), vecptr2.end());
+    std::cout << "Sort time    = " << MySorts::Duration::end() << std::endl;
+
+    // for (auto &v: vecptr)
+    // {
+    //     std::cout << v << std::endl;
+    // }
     return 0;
 }
